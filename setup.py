4cb2734d00600f2f689905579fc0562cbb0c49e8
Ningster <curarning@yahoo.com.tw>
2016-09-05 21:37:54 +0800

    syntax error fixed

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='access_PlaceAPI_tool',
     version=__version__,
     description='This tool is designed to send GET request to google place api in an easier way : '
-                'hide the key and the response is in dictionary format.'
+                'hide the key and the response is in dictionary format.',
     author='Ning Kang',
     maintainer_email='curarning@gmail.com',
     url='https://github.com/Ningster/access_PlaceAPI_tool',
