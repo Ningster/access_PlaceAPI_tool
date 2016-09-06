2ce2329a926f9bbeaab31e0729b4571a7a9bee8e
Ningster <curarning@yahoo.com.tw>
2016-09-06 16:12:43 +0800

    add exception

--- access_PlaceAPI_tool/__init__.py
+++ access_PlaceAPI_tool/__init__.py
@@ -9,4 +9,11 @@ class PlaceAPI:
         paraDict['key']=self.access_token
         result_json = requests.get('https://maps.googleapis.com/maps/api/place'+action+'/json',params=paraDict)
         result_dict= result_json.json()
-        return result_dict
+        if result_dict['status']=='OK':
+            return result_dict
+        else:
+            try:
+                error_message = result_dict["error_message"]
+                raise Exception(error_message)
+            except Exception as e:
+                raise
